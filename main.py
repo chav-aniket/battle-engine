e88bccd8d04a936ae8b13afb0fac5b329113743a
Aniket Chavan <chavaniket@outlook.com>
2020-05-11 16:59:37 +1000

    Added functionalities: Opponent can now use their moves, Working Heal moves, Move log sticking to maximum 5 entries

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 """
-Battle Engine V1.0
+Battle System
 Developed by: Aniket Chavan
 Start Date: 11-05-20
 """
 
 import os
+# import tkinter
+import random
 from threading import Thread
+from signal import signal, SIGINT, SIGTERM
+
+VERSION = 0.1
+
+# tkinter.Tcl().eval('info patchlevel')
 
 class Move:
     '''
@@ -31,7 +38,7 @@ class AttackMove(Move):
 
     def use(self, opponent):
         opponent.take(self.val)
-        return f"{self.owner.name} dealt {self.val} damage!"
+        return f"{self.owner.name} used {self.name} and dealt {self.val} damage!"
 
 class RegenMove(Move):
     '''
@@ -39,14 +46,13 @@ class RegenMove(Move):
     '''
     def __init__(self, name, val):
         super().__init__(name, val)
+        self.name = name
+        self.val = val
     
     def use(self, opponent):
-        self.owner.heal()
-        print(f"{self.owner.name} healed for {self.val}!")
+        self.owner.heal(self.val)
+        return f"{self.owner.name} used {self.name} to heal for {self.val}!"
 
-bitch_slap = AttackMove("Bitch Slap", 30)
-poke = AttackMove("Poke", 10)
-heal = RegenMove("Heal", 10)
 
 class Monster:
     '''
@@ -54,6 +60,8 @@ class Monster:
     '''
     def __init__(self, name):
         self.name = name
+        self.level = 1
+        self.exp = 0
         self.health = 100
         self.attack = 100
         self.defence = 100
@@ -69,16 +77,20 @@ class Monster:
         self.moves.append(move)
         move.addOwner(self)
 
-Chris = Monster("Chris")
-Chris.learnMove(bitch_slap)
-Chris.learnMove(poke)
-Thomas = Monster("Thomas")
-Thomas.learnMove(poke)
+chris = Monster("Chris")
+chris.learnMove(AttackMove("Bitch Slap", 30))
+chris.learnMove(AttackMove("Poke", 10))
+chris.learnMove(RegenMove("Heal", 10))
+
+thomas = Monster("Thomas")
+thomas.learnMove(AttackMove("Poke", 10))
+thomas.learnMove(RegenMove("Heal", 10))
+thomas.learnMove(AttackMove("Bitch Slap", 30))
 
 class Battle(Thread):
     def __init__(self, blue, red):
-        Thread.__init__(self)
         self.turn = 0
+        Thread.__init__(self)
         self.blue = blue
         self.red = red
         self.log = []
@@ -86,33 +98,56 @@ class Battle(Thread):
 
     def check_state(self):
         if self.blue.health <= 0:
-            print('Better luck next time!')
+            print(f"{self.red.name} beat {self.blue.name}, better luck next time!")
             os._exit(0)
+            del self
         elif self.red.health <= 0:
-            print('Congratulations!')
+            print(f"Congratulations, {self.blue.name} beat {self.red.name}!")
             os._exit(0)
+            del self
+    
+    def logUpdate(self, text):
+        while len(self.log) > 5:
+            self.log.pop(0)
+        self.log.append(text)
 
     def start(self):
         while True:
+            os.system('clear')
             print('-------------------------------------------------')
             print(f"Opponent: {self.red.name} - {self.red.health} HP")
             print("\n")
             print(f"You: {self.blue.name} - {self.blue.health} HP")
             print("\n")
             print("===")
+            for line in self.log:
+                print(line)
+            print("===")
             print("These are your moves - ")
             for count, move in enumerate(self.blue.moves):
                 print(f"{count+1}. {move.name}")
-            try:
-                curr_move = int(input("Enter move number: "))
-            except ValueError:
-                print("Please enter a valid number")
-                curr_move = int(input("Enter move number:"))
-            self.log.append(self.blue.moves[curr_move - 1].use(self.red))
+            while True:
+                try:
+                    curr_move = int(input("Enter move number: "))
+                    if curr_move > len(self.blue.moves):
+                        raise ValueError
+                except ValueError:
+                    print("Please enter a valid number")
+                else:
+                    break
+            self.logUpdate(self.blue.moves[curr_move-1].use(self.red))
+            self.logUpdate(self.red.moves[random.randint(0, len(self.red.moves)-1)].use(self.blue))
             # os.system('clear')
-            for line in self.log:
-                print(line)
             self.check_state()
 
+def exitProgram(signal, frame):
+    print("\nExiting Battle System")
+    os._exit(0)
+
+# window = Tk()
+# window.title("Battle System ")
+
 if __name__ == "__main__":
-    new = Battle(Chris, Thomas)
+    signal(SIGINT, exitProgram)
+    signal(SIGTERM, exitProgram)
+    new = Battle(chris, thomas)
