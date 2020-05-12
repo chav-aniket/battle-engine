58558d34e74f739dbc28f0b8b1b48286a93b562e
Aniket Chavan <chavaniket360@protonmail.com>
2020-05-12 15:46:32 +1000

    Allows user to choose between Turtwig and Chimchar

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ import os
 # import tkinter
 from signal import signal, SIGINT, SIGTERM
 
-from classes import Battle, Monster, Move
-from monsters import Chris, Thomas
-from error import useError
+from classes import Battle
+from monsters import *
 
 VERSION = 0.1
 # tkinter.Tcl().eval('info patchlevel')
-
-chris = Chris()
-thomas = Thomas()
 
 def exitProgram(signal, frame):
     print("\nExiting Battle System")
@@ -28,4 +24,19 @@ def exitProgram(signal, frame):
 if __name__ == "__main__":
     signal(SIGINT, exitProgram)
     signal(SIGTERM, exitProgram)
-    Battle(chris, thomas)
+    while True:
+        try:
+            print("Which Pokemon would you like to use?")
+            print("Turtwig (1) or Chimchar (2)")
+            user = int(input("Enter number: "))
+            if user > 2:
+                raise ValueError
+        except ValueError:
+            os.system('clear')
+            print("Please input a valid number")
+        else:
+            break
+    if user:
+        Battle(Turtwig(), Chimchar())
+    else:
+        Battle(Chimchar(), Turtwig())
