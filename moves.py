f85237a7fc3827a8a2af429798f8bc88d7550899
Aniket Chavan <chavaniket360@protonmail.com>
2020-05-12 15:47:04 +1000

    moves.py: Replaced old moves with placeholder pokemon moves for testing purposes

--- moves.py
+++ moves.py
@@ -6,34 +6,51 @@ Includes:
     - Use Error Class
 """
 
-from classes import Move
+from classes import Element, Move
 
-class Poke(Move):
+class Leer(Move):
     def __init__(self):
         super().__init__(
-            name="Poke", 
-            element=None, 
+            name="Leer",  
+            cat=Move.STAT, 
+            uses=30
+            )
+
+class Scratch(Move):
+    def __init__(self):
+        super().__init__(
+            name="Scratch", 
             cat=Move.PHYS, 
-            dmg=10, 
+            dmg=40,  
+            uses=35
+            )
+
+class Tackle(Move):
+    def __init__(self):
+        super().__init__(
+            name="Tackle", 
+            cat=Move.PHYS, 
+            dmg=40,  
+            uses=35
+            )
+
+class Absorb(Move):
+    def __init__(self):
+        super().__init__(
+            name="Absorb", 
+            element=Element.GRASS, 
+            cat=Move.PHYS, 
+            dmg=25, 
+            heal=0.65*25, 
             uses=25
             )
 
-class BitchSlap(Move):
+class Ember(Move):
     def __init__(self):
         super().__init__(
-            name="Bitch Slap", 
-            element=None, 
-            cat=Move.PHYS, 
-            dmg=30,  
-            uses=20
-            )
-
-class Heal(Move):
-    def __init__(self):
-        super().__init__(
-            name="Heal", 
-            element=None, 
-            cat=Move.STAT, 
-            heal=20,  
-            uses=20
+            name="Ember", 
+            element=Element.FIRE, 
+            cat=Move.SPEC, 
+            dmg=40,  
+            uses=25
             )
